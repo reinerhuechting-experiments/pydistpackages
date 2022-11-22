ec984082385aeda9cd77bda45e5121b50279d2a9
Reiner Hüchting <dev@reinerhuechting.de>
2022-11-22 12:23:35 +0100

    Extend package2.module2 with a function that imports something from another module in the same package.

--- package2/module2.py
+++ package2/module2.py
@@ -1,2 +1,8 @@
 def foo():
     print("This is foo() from module2 in package2.")
+
+def bar():
+    print("This is bar() from module2 in package2.")
+    print("Calling a function from module in package2...")
+    from . import module1
+    module1.foo()
