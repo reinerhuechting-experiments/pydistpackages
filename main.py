aa7c837a475a3fab1e3a18b47ec9f176a74dadb4
Reiner Hüchting <dev@reinerhuechting.de>
2022-11-22 12:07:12 +0100

    Add two packages and a main that plays around with them.

---
+++ main.py
@@ -0,0 +1,18 @@
+#%%
+from package1.module1 import foo as foo11
+
+#%%
+foo11()
+
+#%%
+import package1.module2
+
+# %%
+package1.module2.foo()
+
+# %%
+import package2.module1
+
+# %%
+package2.module1.foo()
+# %%
---
+++ package1/module2.py
@@ -0,0 +1,2 @@
+def foo():
+    print("This is foo() from module2 in package1.")
